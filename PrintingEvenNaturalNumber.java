6000d363a487d498bf66081770c571ceeba0874b
Anshika Singh <43119152+anshikaX@users.noreply.github.com>
2020-04-22 15:51:01 +0530

    Add files via upload

---
+++ PrintingEvenNaturalNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class PrintingEvenNaturalNumber {
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int number = sc.nextInt();
+        for (int i =1; i<=number; i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i+ " ");
+            }
+        }
+
+
+    }
+}
+
